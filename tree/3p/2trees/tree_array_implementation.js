056b332abd341fc874acab55ef47fd8527de09f6
Gustavo Ceb <66754601+GusCeb123@users.noreply.github.com>
2021-11-29 19:22:31 -0600

    Update tree_array_implementation.js

--- tree/3p/2trees/tree_array_implementation.js
+++ tree/3p/2trees/tree_array_implementation.js
@@ -1,1 +1,34 @@
+//CeballosMirandaGustavo
+//Estructura de datos
+function Node(obj){
+    this.data = obj;  
+    this.left = null; 
+    this.right = null;
+}
+//Arreglo S
+var s = [];
+  //Nodos
+  s[0] = new Node('+');
+  s[1] = new Node(30);
+  s[2] = new Node(40);
+  s[3] = new Node(50);
+  s[4] = new Node(60);
+  s[5] = new Node(70);
+  s[6] = new Node(80);
+    //Hijos
+    s[0].left = s[1];
+    s[0].right = s[2];
+    s[1].left = s[3];
+    s[1].right = s[4];
+    s[2].left = s[5];
+    s[2].right = s[6];
 
+      console.log("Inorder");
+        inorder(s[0]); //OrdenSimetrico
+          function inorder(n){
+            if (n!==null){
+              inorder(n.left);
+            console.log(n.data);
+        inorder(n.right);
+    }
+}
