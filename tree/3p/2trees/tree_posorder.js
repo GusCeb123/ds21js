49bbb429e29558b14e8a0dc2a7232f0cf4966439
Gustavo Ceb <66754601+GusCeb123@users.noreply.github.com>
2021-11-29 19:26:35 -0600

    Update tree_posorder.js

--- tree/3p/2trees/tree_posorder.js
+++ tree/3p/2trees/tree_posorder.js
@@ -1,1 +1,23 @@
-
+//CeballosMirandaGustavo
+//Estructura de Datos
+function Node(obj) {
+    this.data = obj;  
+    this.left = null; 
+    this.right = null;
+}
+//Nodos
+let s = new Node('+');
+let s1 = new Node(14);
+let s2 = new Node(15);
+//Hijos
+s.left = s1;
+s.right = s2;
+console.log("Posorder");
+posorder(s); //Posorder - Orden Posterior
+function posorder(r) {
+    if (r!==null) {
+        posorder(r.left);
+        posorder(r.right);
+        console.log(r.data);
+    }
+}
