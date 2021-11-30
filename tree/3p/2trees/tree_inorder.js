6f8a81e374414032d2283c2143cc966089d80a12
Gustavo Ceb <66754601+GusCeb123@users.noreply.github.com>
2021-11-29 19:24:28 -0600

    Update tree_inorder.js

--- tree/3p/2trees/tree_inorder.js
+++ tree/3p/2trees/tree_inorder.js
@@ -1,1 +1,24 @@
+//CeballosMirandaGustavo
+//Estructura de Datos
+function Node(obj){
+    this.data = obj;  
+    this.left = null; 
+    this.right = null;
+}
+//Nodos
+let s = new Node('+');
+let s1 = new Node(14);
+let s2 = new Node(15);
+//Hijos
+s.left = s1;
+s.right = s2;
 
+console.log("Inorder");
+inorder(s); //OrdenSimetrico o Inorder
+function inorder(n){
+    if (n!==null){
+        inorder(n.left);
+        console.log(n.data);
+        inorder(n.right);
+    }
+}
