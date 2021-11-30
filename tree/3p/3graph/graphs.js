ff49ab6a166fc5656b1cb1b6998b8e8bd90a86aa
Gustavo Ceb <66754601+GusCeb123@users.noreply.github.com>
2021-11-29 19:30:07 -0600

    Update graphs.js

--- tree/3p/3graph/graphs.js
+++ tree/3p/3graph/graphs.js
@@ -1,1 +1,38 @@
+//CeballosMirandaGustavo
+function Graph(z) {
+    this.vertices = z;
+    this.edges = 0;
+    this.adj = [];
+    for (var i = 0; i < this.vertices; ++i) {
+        this.adj[i] = [];
+        this.adj[i].push("");
+    }
+    this.addEdge = addEdge;
+    this.showGraph = showGraph;
+}
+function addEdge(z, y) {
+    this.adj[z].push(y);
+    this.adj[y].push(z);
+    this.edges++;
+}
+function showGraph() {
+    for (var i = 0; i < this.vertices; ++i) {
+        console.log(i + " -> ");
+        for (var j = 0; j < this.vertices; ++j) {
+            if (this.adj[i][j] != undefined)
+                console.log(this.adj[i][j] + ' ');
+        }
+    }
+}
+var s = new Graph(5);
+s.addEdge(0,1);
+s.addEdge(0,2);
+s.addEdge(1,3);
+s.addEdge(2,4);
+s.showGraph();
 
+
+
+
+
+//AuxiliarPrincipal https://www.geeksforgeeks.org/implementation-graph-javascript/
